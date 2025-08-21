37e1a2d7438ba28aa93c61f75dbe7c81f2ad65e4
iamdudumon <kksy4511@naver.com>
2025-08-21 14:30:41 +0900

    chore: 사용하지 않는 쉘 파일 삭제

--- agent/exec-compose.bash
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-grpcurl -plaintext -import-path . -proto ./proto/runner_agent.proto -d \
-	'{"job_id": "exp-2", "cpu": 1.5, "mem": "512M"}' \
-	localhost:50051 runner.RunnerAgent/RunExperiment
